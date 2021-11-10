22871ddc4acca4b5585030d0987e0573f19634b9
veneta13 <venkireva80@gmail.com>
2021-11-10 10:14:03 +0200

    clear data modeling tasks

--- solutions/2-data-modeling/predicates.py
+++
@@ -1,5 +0,0 @@
-# digit = oftype(int) & gt(-1) & lt(10)
-# binary = eq(0) | eq(1)
-# number = for_any(oftype(int), oftype(float), oftype(complex))
-# is_the_empty_string = pred(lambda x: x is "")
- 
