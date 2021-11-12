11435c14b42ee092a6469b051774ca0fbac5a47e
veneta13 <venkireva80@gmail.com>
2021-11-12 14:20:34 +0200

    decorators-log with lambda func

--- solutions/2-data-modeling/decorators-log.py
+++ solutions/2-data-modeling/decorators-log.py
@@ -2,31 +2,28 @@ from datetime import datetime
 
 def encrypt(key):
     def inner_function(function):
-        def wrapper():
-            text = function()
-            new_text = ""
-            for character in text:
-                if character.islower():
-                    new_text += chr((ord(character) + key - 97) % 26 + 97)
-                elif character.isupper():
-                    new_text += chr((ord(character) + key - 65) % 26 + 65)
-                else:
-                    new_text += character
-            return new_text
-        wrapper.__name__ = function.__name__ # to print the function name in file
-        return wrapper
+        text = function()
+        new_text = ""
+        for character in text:
+            if character.islower():
+                new_text += chr((ord(character) + key - 97) % 26 + 97)
+            elif character.isupper():
+                new_text += chr((ord(character) + key - 65) % 26 + 65)
+            else:
+                new_text += character
+        return_func = lambda x = new_text: x # to save function name
+        return_func.__name__ = function.__name__
+        return return_func
     return inner_function
 
 def log(file_name):
-        def inner_function(function):
-            def wrapper():
-                text = function()
-                file = open(file_name, "a")
-                file.write("{} was called at {} \n".format(function.__name__, datetime.now()))
-                file.close()
-                return text
-            return wrapper
-        return inner_function
+    def inner_function(function):
+        text = function()
+        file = open(file_name, "a")
+        file.write("{} was called at {} \n".format(function.__name__, datetime.now()))
+        file.close()
+        return lambda x = text: x
+    return inner_function
 
 @log('log.txt')
 @encrypt(2)
