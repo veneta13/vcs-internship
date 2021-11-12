188e211eea0e23ebf072aa004c0df0df2dc6bdb4
veneta13 <venkireva80@gmail.com>
2021-11-12 14:09:40 +0200

    decorators-encrypt with lambda func

--- solutions/2-data-modeling/decorators-encrypt.py
+++ solutions/2-data-modeling/decorators-encrypt.py
@@ -1,19 +1,16 @@
 def encrypt(key):
     def inner_function(function):
-        def wrapper():
-            text = function()
-            new_text = ""
-            for character in text:
-                if character.islower():
-                    new_text += chr((ord(character) + key - 97) % 26 + 97)
-                elif character.isupper():
-                    new_text += chr((ord(character) + key - 65) % 26 + 65)
-                else:
-                    new_text += character
-            return new_text
-        return wrapper
+        text = function()
+        new_text = ""
+        for character in text:
+            if character.islower():
+                new_text += chr((ord(character) + key - 97) % 26 + 97)
+            elif character.isupper():
+                new_text += chr((ord(character) + key - 65) % 26 + 65)
+            else:
+                new_text += character
+        return lambda x = new_text: x
     return inner_function
-
 
 @encrypt(2)
 def get_low():
