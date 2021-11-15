359e9242c229b5838b260c28ecf86c1b27a527a7
veneta13 <venkireva80@gmail.com>
2021-11-15 12:05:25 +0200

    linked list implementation without length

--- 02_data_modeling/linkedlist.py
+++ 02_data_modeling/linkedlist.py
@@ -13,47 +13,67 @@ class LinkedListIterator:
             self.current = self.current.next
             return item
 
+    def next(self):
+        return self.current.next.data
+
 class Cons:
     
     def __init__(self, data, next = None):
         self.data = data
         self.next = next
-        self.lenght = 0 if next is None else next.lenght + 1
 
-    def __getitem__(self, key, counter = 0):
-        if key != counter:
-            if key < 0:
-                key = self.lenght + key
-            return self.next.__getitem__(key, counter + 1)
-        else: 
-            return self.data
+    def __getitem__(self, key):
+        itemCount = self.__countItems()
         
+        if key < 0:
+            key = itemCount + key
+        if key >= itemCount:
+            raise IndexError("Index out of range")
+        
+        current = self
+        counter = 0
+        while counter != key:
+            current = current.next
+            counter += 1
+        return current.data
+
     def __iter__(self):
         return LinkedListIterator(self)
+
+    def __countItems(self):
+        current = self
+        counter = 1
+        while current.next.data is not None:
+            current = current.next
+            counter += 1
+        return counter
 
 nil = Cons(None)
 
 def make_list(*items):
     cons_list = [Cons(item) for item in items]
     cons_list.append(Cons(None, None))
-    
-    lenght_counter = 1
     for following, current in zip(cons_list[::-1], cons_list[-2::-1]):
         current.next = following
-        current.lenght = lenght_counter
-        lenght_counter += 1
-        
     return cons_list[0]
 
 # l = Cons(1, Cons(2, Cons(3, Cons(4, Cons(5, nil)))))
-# print("l[1] = ", l[0])
-# print("l[1] = ", l[-1])
+# print("l[4] = ", l[4])
+# print("l[0] = ", l[0])
+# print("l[-4] = ", l[-4])
+
+# myiter = iter(l)
+# print("myiter.next() = ", myiter.next())
+
+# print("next(myiter) = " ,next(myiter))
+# print("next(myiter) = " ,next(myiter))
+
 # l_iter_list = [li + 1 for li in l]
 # print("l iter list:", l_iter_list)
 
 # m = make_list(1, 2, 3, 4, 5)
-# print("m[1] = ", m[0])
-# print("m[1] = ", m[-1])
+# print("m[1] = ", m[1])
+# print("m[-1] = ", m[-1])
 # m_iter_list = [li + 1 for li in m]
 # print("m iter list:", m_iter_list)
 
