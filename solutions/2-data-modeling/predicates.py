58f80fcfba71ea5eba3b6f1056843d329ee2e3ba
veneta13 <venkireva80@gmail.com>
2021-11-24 14:00:01 +0200

    predicates without extra functions

--- solutions/2-data-modeling/predicates.py
+++ solutions/2-data-modeling/predicates.py
@@ -3,34 +3,26 @@ class Predicate:
     def __init__(self, func=None):
         self.func = func
 
-    def __get_func(self, x):
-        if (self.func == present_func):
-            return self.func()
-        return lambda y = None: self.func(x) if y is None else self.func(x)(y)
-
     def __call__(self, x=None):
-        return Predicate(self.__get_func(x))
-
-    def __bool__(self):
-        return self.func()
+        return self.func(x)
 
     def __and__(self, other):
-        return Predicate(lambda y: self.func(y) and other.func(y))
+        return lambda x: self(x) & other(x)
 
     def __rand__(self, other):
-        return Predicate(lambda y: other.func(y) and self.func(y))
+        return lambda x: other(x) & self(x)
 
     def __or__(self, other):
-        return Predicate(lambda y: self.func(y) or other.func(y))
+        return lambda x: self(x) | other(x)
 
     def __ror__(self, other):
-        return Predicate(lambda y: other.func(y) or self.func(y))
+        return lambda x: other(x) | self(x)
 
     def __invert__(self):
-        return Predicate(lambda y: self.func(y) is False)
+        return lambda x: not self(x)
 
     def __rshift__(self, other):
-        return Predicate(lambda y: not(self.func(y)) or other.func(y))
+        return lambda x: not(self.func(x)) or other.func(x)
 
 
 def for_any(*preds):
@@ -41,33 +33,25 @@ def for_all(*preds):
     return lambda y: all(item.func(y) for item in preds)
 
 
-def gt_func(y):
-    return lambda x: x > y
+def gt(x):
+    return Predicate(lambda y: y > x)
 
 
-def lt_func(y):
-    return lambda x: x < y
+def lt(x):
+    return Predicate(lambda y: y < x)
 
 
-def eq_func(y):
-    return lambda x: x == y
+def eq(x):
+    return Predicate(lambda y: y == x)
 
 
-def oftype_func(t):
-    return lambda x: isinstance(x, t)
+def oftype(t):
+    return Predicate(lambda x: isinstance(x, t))
 
 
-def present_func():
-    return lambda x: x is not None
+def present():
+    return Predicate(lambda x: x is not None)
 
 
-def pred_func(func):
-    return lambda x: func(x)
-
-
-gt = Predicate(gt_func)
-lt = Predicate(lt_func)
-eq = Predicate(eq_func)
-oftype = Predicate(oftype_func)
-present = Predicate(present_func)
-pred = Predicate(pred_func)
+def pred(func):
+    return Predicate(lambda x: func(x))
