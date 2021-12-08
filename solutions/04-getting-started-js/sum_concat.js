04a16594d7843ae9e9d5843643aebc112fe6d077
Veneta <36812226+veneta13@users.noreply.github.com>
2021-12-08 18:22:58 +0200

    JS Tasks: sum concat (#11)
    * task: sum-concat
    
    * use strict equality in functions
    
    * added install eslint standard
    
    * fix eslint workflow
    
    * eslint manual config
    
    * update eslint configuration
    
    * lint code
    
    * added blocks in else statements
    
    * added removed --save-dev from actions

---
+++ solutions/04-getting-started-js/sum_concat.js
@@ -0,0 +1,18 @@
+function sum (a, b) {
+  if (typeof a === 'number' && typeof b === 'number') {
+    return a + b
+  } else {
+    throw new TypeError('Error: arguments of sum should be numbers.')
+  }
+}
+
+function concat (a, b) {
+  if (typeof a === 'string' && typeof b === 'string') {
+    return a + b
+  } else {
+    throw new TypeError('Error: arguments of concat should be string.')
+  }
+}
+
+sum(1, 2)
+concat('1', '2')
