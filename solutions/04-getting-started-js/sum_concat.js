8d2d5ee3e2ab1135ee5934feae7aca93e5f79f56
veneta13 <venkireva80@gmail.com>
2021-12-08 12:33:58 +0200

    lint code

--- solutions/04-getting-started-js/sum_concat.js
+++ solutions/04-getting-started-js/sum_concat.js
@@ -1,11 +1,14 @@
-function sum(a, b) {
+function sum (a, b) {
   if (typeof a === 'number' && typeof b === 'number') {
-    return a + b;
-  } else throw new TypeError('Error: arguments of sum should be numbers.');
+    return a + b
+  } else throw new TypeError('Error: arguments of sum should be numbers.')
 }
 
-function concat(a, b) {
+function concat (a, b) {
   if (typeof a === 'string' && typeof b === 'string') {
-    return a + b;
-  } else throw new TypeError('Error: arguments of concat should be string.');
+    return a + b
+  } else throw new TypeError('Error: arguments of concat should be string.')
 }
+
+sum(1, 2)
+concat('1', '2')
