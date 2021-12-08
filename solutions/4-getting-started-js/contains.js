49aee2eac2466a8b49c611650d8add39ca182a97
veneta13 <venkireva80@gmail.com>
2021-12-08 19:02:41 +0200

    removed undefined comparison

--- solutions/4-getting-started-js/contains.js
+++ solutions/4-getting-started-js/contains.js
@@ -1,6 +1,6 @@
 // eslint-disable-next-line no-var
 var contains = function (element, arr) {
-  return arr.find(item => item === element) !== undefined
+  return Boolean(arr.find(item => item === element))
 }
 
 console.log(contains(1, [1, 2, 3]))
