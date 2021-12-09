b95b396a4172a14a0a0c9fb6dff28440147a80cb
veneta13 <venkireva80@gmail.com>
2021-12-09 12:24:49 +0200

    only with every

--- solutions/4-getting-started-js/only.js
+++ solutions/4-getting-started-js/only.js
@@ -1,7 +1,6 @@
 /* eslint-disable valid-typeof */
-// eslint-disable-next-line no-var
 var only = function (type, arr) {
-  return (arr.filter(item => (typeof item) === type)).length === arr.length
+  return arr.every(item => (typeof item) === type)
 }
 
 console.log(only('string', [1, 2, 3, 4])) // false
