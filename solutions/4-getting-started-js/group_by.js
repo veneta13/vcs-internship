2430c6f84346eee8e3297c67b53a2f237da2cb90
Veneta <36812226+veneta13@users.noreply.github.com>
2021-12-09 13:34:26 +0200

    JS Tasks: groupBy (#16)
    * groupBy task
    
    * groupBy with reduce

---
+++ solutions/4-getting-started-js/group_by.js
@@ -0,0 +1,29 @@
+// eslint-disable-next-line no-var
+var groupBy = function (groupingFunction, arr) {
+  return arr.reduce((result, item) => {
+    const value = item[Object.keys(item).find(key => item[key] === groupingFunction(item))]
+    result[value] = (result[value] || []).concat(item)
+    return result
+  }, {})
+}
+
+const students = [{
+  name: 'Daniel Taskoff',
+  course: 'Frontend JavaScript'
+}, {
+  name: 'Elena Jeleva',
+  course: 'Programming 101'
+}, {
+  name: 'Luboslava Dimitrova',
+  course: 'Frontend JavaScript'
+}, {
+  name: 'Anton Antonov',
+  course: 'Core Java'
+}, {
+  name: 'Nikola Dichev',
+  course: 'Core Java'
+}]
+
+console.log(groupBy(function (student) {
+  return student.course
+}, students))
