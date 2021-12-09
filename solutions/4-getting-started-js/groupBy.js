1204c5f41fe1b1d57b05f83b1e6c7c9f3493ab60
veneta13 <venkireva80@gmail.com>
2021-12-09 14:25:49 +0200

    edit groupBy

--- solutions/4-getting-started-js/groupBy.js
+++ solutions/4-getting-started-js/groupBy.js
@@ -7,25 +7,4 @@ var groupBy = function (groupingFunction, arr) {
   }, {})
 }
 
-const students = [{
-  name: 'Daniel Taskoff',
-  course: 'Frontend JavaScript'
-}, {
-  name: 'Elena Jeleva',
-  course: 'Programming 101'
-}, {
-  name: 'Luboslava Dimitrova',
-  course: 'Frontend JavaScript'
-}, {
-  name: 'Anton Antonov',
-  course: 'Core Java'
-}, {
-  name: 'Nikola Dichev',
-  course: 'Core Java'
-}]
-
-console.log(groupBy(function (student) {
-  return student.course
-}, students))
-
 export { groupBy }
