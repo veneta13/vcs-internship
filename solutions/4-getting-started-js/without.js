3a56deb4cf708aaff5eb7239fca9c83fb5f104b7
veneta13 <venkireva80@gmail.com>
2021-12-09 14:58:22 +0200

    without no forEach

--- solutions/4-getting-started-js/without.js
+++ solutions/4-getting-started-js/without.js
@@ -1,8 +1,5 @@
 var without = function (exclude, arr) {
-  exclude.forEach(excItem => {
-    arr = arr.filter(arrItem => excItem !== arrItem)
-  })
-  return arr
+  return arr.filter(arrItem => !exclude.includes(arrItem))
 }
 
 console.log(without([5, 6], [1, 2, 3, 4, 5, 6])) // [1,2,3,4]
