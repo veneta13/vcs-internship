1063f3708302b2ba32d3ca6a00b3921887cd5272
veneta13 <venkireva80@gmail.com>
2022-03-01 11:17:57 +0200

    reformat tests

--- solutions/6-django/backend/conftest.py
+++ solutions/6-django/backend/conftest.py
@@ -21,57 +21,75 @@ def client(user):
 
 @pytest.fixture
 def user():
-    return User.objects.create(username='tester', password='123456')
+    return User.objects.create(
+        username='tester',
+        password='123456'
+    )
 
 
 @pytest.fixture
 def other_user():
-    return User.objects.create(username='other', password='123456')
+    return User.objects.create(
+        username='other',
+        password='123456'
+    )
 
 
 @pytest.fixture
 def link():
-    return Link.objects.create(link='http://www.google.com')
+    return Link.objects.create(
+        link='http://www.google.com'
+    )
 
 
 @pytest.fixture
 def link2():
-    return Link.objects.create(link='http://www.bing.com')
+    return Link.objects.create(
+        link='http://www.bing.com'
+    )
 
 
 @pytest.fixture
 def empty_list(user):
-    return LinkList.objects.create(owner=user,
-                                   name='Test List',
-                                   description='Test Description',
-                                   public=True)
+    return LinkList.objects.create(
+      owner=user,
+      name='Test List',
+      description='Test Description',
+      public=True
+    )
 
 
 @pytest.fixture
 def list(user, link):
-    llist = LinkList.objects.create(owner=user,
-                                    name='Test List',
-                                    description='Test Description',
-                                    public=True)
+    llist = LinkList.objects.create(
+      owner=user,
+      name='Test List',
+      description='Test Description',
+      public=True
+    )
     llist.links.set([link])
     return llist
 
 
 @pytest.fixture
 def other_private_list(other_user, link2):
-    llist = LinkList.objects.create(owner=other_user,
-                                    name='Other List',
-                                    description='Other Description',
-                                    public=False)
+    llist = LinkList.objects.create(
+        owner=other_user,
+        name='Other List',
+        description='Other Description',
+        public=False
+    )
     llist.links.set([link2])
     return llist
 
 
 @pytest.fixture
 def other_public_list(other_user, link):
-    llist = LinkList.objects.create(owner=other_user,
-                                    name='Other Public List',
-                                    description='Other Description',
-                                    public=True)
+    llist = LinkList.objects.create(
+        owner=other_user,
+        name='Other Public List',
+        description='Other Description',
+        public=True
+    )
     llist.links.set([link])
     return llist
--- solutions/6-django/backend/llists/tests.py
+++ solutions/6-django/backend/llists/tests.py
@@ -4,10 +4,12 @@ import conftest
 
 
 @pytest.mark.django_db
-def test_get_list_test(client, list):
+def test_list_lists(client, list):
     response = client.get(reverse('lists-list'))
     assert response.status_code == 200
     assert response.data['results'][0]['name'] == 'Test List'
+    assert response.data['results'][0]['description'] == 'Test Description'
+    assert response.data['results'][0]['public'] is True
 
 
 @pytest.mark.django_db
@@ -18,8 +20,7 @@ def test_get_own_list(client, list):
     assert response.data.get('name') == 'Test List'
     assert response.data.get('public') is True
     assert response.data.get('links')[0].get('link') == 'http://www.google.com'
-    assert response.data.get(
-        'description') == 'Test Description'
+    assert response.data.get('description') == 'Test Description'
 
 
 @pytest.mark.django_db
@@ -34,6 +35,9 @@ def test_get_public_list(client, list, other_public_list):
     url = reverse('lists-detail', kwargs={'pk': 2})
     response = client.get(url)
     assert response.status_code == 200
+    assert response.data.get('name') == 'Other Public List'
+    assert response.data.get('description') == 'Other Description'
+    assert response.data.get('public') is True
 
 
 @pytest.mark.django_db
@@ -42,11 +46,14 @@ def test_create_empty_public_list(client):
         reverse('lists-list'),
         {
             'name': 'My public test list',
-            'description': 'This is my test list description',
+            'description': 'Test list description',
             'public': True
         }
     )
     assert response.status_code == 201
+    assert response.data.get('name') == 'My public test list'
+    assert response.data.get('description') == 'Test list description'
+    assert response.data.get('public') is True
 
 
 @pytest.mark.django_db
@@ -54,34 +61,38 @@ def test_create_empty_private_list(client):
     response = client.post(
         reverse('lists-list'),
         {
-            'name': 'My public test list',
-            'description': 'This is my test list description',
+            'name': 'My private test list',
+            'description': 'Test list description',
             'public': False
         }
     )
     assert response.status_code == 201
+    assert response.data.get('name') == 'My private test list'
+    assert response.data.get('description') == 'Test list description'
+    assert response.data.get('public') is False
 
 
 @pytest.mark.django_db
-def test_create_public_list_with_links(client):
+def test_add_links_to_public_list(client):
     response = client.post(
         reverse('lists-list'),
         {
             'name': 'My public test list',
-            'description': 'This is my test list description',
+            'description': 'Test list description',
             'public': True
         }
     )
     assert response.status_code == 201
 
     links = ['https://www.google.com', 'https://www.example.com']
+
     for currentLink in links:
         new_resp = client.patch(response.data['url'], {'link': currentLink})
         assert new_resp.status_code == 200
 
 
 @pytest.mark.django_db
-def test_create_private_list_with_links(client):
+def test_add_links_to_private_list(client):
     response = client.post(
         reverse('lists-list'),
         {
@@ -93,6 +104,7 @@ def test_create_private_list_with_links(client):
     assert response.status_code == 201
 
     links = ['https://www.google.com', 'https://www.example.com']
+
     for currentLink in links:
         new_resp = client.patch(response.data['url'], {'link': currentLink})
         assert new_resp.status_code == 200
